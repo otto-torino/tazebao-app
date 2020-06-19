ac38724f8212b5187fc1c2fc4d6d9468b1c60115
abidibo <abidibo@gmail.com>
2020-06-19 14:05:04 +0200

    fixes campaigns timeout error message

--- src/Sagas/CampaignsSagas.js
+++ src/Sagas/CampaignsSagas.js
@@ -4,10 +4,11 @@ import CampaignsActions from '../Redux/Campaigns'
 export function * fetchCampaigns (api, { payload }) {
   // request
   const response = yield call(api.campaigns)
+  console.log('CAZZO', response)
   // success?
   if (response.ok) {
     yield put(CampaignsActions.campaignsSuccess(response.data))
   } else {
-    yield put(CampaignsActions.campaignsFailure({ code: response.status, detail: response.data.detail }))
+    yield put(CampaignsActions.campaignsFailure({ code: response.status, detail: response.data ? response.data.detail : response.problem }))
   }
 }
