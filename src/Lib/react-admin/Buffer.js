5c1a96b652d461277d87d643d1fa768d98e613cf
Daniele Pira <daniele.pira@otto.to.it>
2020-10-15 17:04:32 +0200

    refactor buffer

--- src/Lib/react-admin/Buffer.js
+++ src/Lib/react-admin/Buffer.js
@@ -1,60 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { backgroundRequest } from '../../Services/Request';
-import { useBuffer as useBuffer2 } from './Buffer2';
+import { useState, useEffect } from 'react'
 
-
-function djangoSaucePageRequest(endpoint, pageSize) {
-
-    return async function(next) {
-        const response = await backgroundRequest(endpoint, [next, pageSize]);
-        const data = response.data.results;
-        next = response.data.next;
-        next = next ? (new URL(next)).searchParams.get("page") : null;
-        return {data, next};
-    }
+function useBuffer (request, start, init, update, delayTime = 0) {
+  const [buffer, setBuffer] = useState(init)
+  const [next, setNext] = useState(start)
+  useEffect(() => {
+    if (next !== null) {
+      request(next)
+        .then(result => {
+          setBuffer(update(buffer, result.data))
+          setNext(result.next)
+        })
+        .catch(console.log) // TODO: manage error
+    } // TODO: maybe manage component destruction trough useEffect return
+  }, [next])
+  return buffer
 }
 
-
-function useBuffer(request, start, init, update, delayTime=0) {
-
-    const [buffer, setBuffer] = useState(init);
-    const [next, setNext] = useState(start);
-
-    useEffect(() => {
-        if(next !== null) {
-            request(next).then(result => {
-                setBuffer(update(buffer, result.data));
-                setNext(result.next); 
-            });
-        }
-    }, [next]);
-
-    return buffer;
+export function useBufferArray (request, start, delayTime = 0) {
+  const init = []
+  const update = (buffer, data) => [...buffer, ...data]
+  return useBuffer(request, start, init, update, delayTime)
 }
 
-
-function useBufferArray(request, start, delayTime=0) {
-    const init = [];
-    const update = (buffer, data) => [...buffer, ...data];
-    return useBuffer(request, start, init, update, delayTime=0);
+export function useBufferObject (request, start, delayTime = 0) {
+  const init = {}
+  const update = (buffer, data) => ({ ...buffer, ...data })
+  return useBuffer(request, start, init, update, delayTime)
 }
 
-
-function useBufferObject(request, start, delayTime=0) {
-    const init = {};
-    const update = (buffer, data) => ({...buffer, ...data});
-    return useBuffer(request, start, init, update, delayTime=0);
-}
-
-
-const Buffer = () => {
-
-    const request = djangoSaucePageRequest('subscribers', 10000);
-    const update = (oldData, newData) => [...oldData, ...newData];
-    const buffer = useBufferArray(request, 1, [], update, 0);
-
-    return <div></div>;
-}
-
-
-export default Buffer;
+export default useBuffer
--- src/Services/Request.js
+++ src/Services/Request.js
@@ -3,16 +3,26 @@ import { toast } from 'react-toastify'
 import { toastOnError } from '../Decorators/Api'
 
 export function request (endpoint, args, errorMessage, onSuccess, onError) {
-    return api[endpoint](...args)
+  return api[endpoint](...args)
     .then(toastOnError(response => {
-        onSuccess(response)
+      onSuccess(response)
     }, errorMessage, onError))
     .catch(error => {
-        toast(errorMessage.replace('{error}', error), { type: 'error' })
-        onError && onError(error)
+      toast(errorMessage.replace('{error}', error), { type: 'error' })
+      onError && onError(error)
     })
 }
 
-export function backgroundRequest(endpoint, args) {
-    return api[endpoint](...args);
+export function backgroundRequest (endpoint, args) {
+  return api[endpoint](...args)
+}
+
+export function djangoSaucePageRequest (endpoint, pageSize) {
+  return async function (next) {
+    const response = await backgroundRequest(endpoint, [next, pageSize])
+    const data = response.data.results
+    next = response.data.next
+    next = next ? (new URL(next)).searchParams.get('page') : null
+    return { data, next }
+  }
 }
