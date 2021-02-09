3fb2c9f5dcadaa97acf9fd8f94f5d75e6bce31db
abidibo <abidibo@gmail.com>
2021-02-09 16:54:14 +0100

    avoids mosaico reloading after token refresh

--- src/Components/Mosaico.js
+++ src/Components/Mosaico.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import config from '../Config'
 
@@ -8,32 +8,46 @@ window.addEventListener('message', function ({ data }) {
   }
 })
 
-const Mosaico = props => {
-  const token = useSelector(state => state.auth.token)
+class Mosaico extends React.Component {
+  shouldComponentUpdate (nextProps) {
+    // if only token has been refreshed do not upload, otherwise the iframe is rendered again and reloads
+    if (nextProps.token && this.props.token && this.props.token !== nextProps.token) {
+      return false
+    }
+    return true
+  }
 
-  const src =
-    props.template && props.template.id
-      ? `${config.mosaicoBasePath}/appeditor.html?bearer=${token}&id=${props.template.id}`
-      : `${config.mosaicoBasePath}/app.html?bearer=${token}`
+  render () {
+    const { token, windowRef, template } = this.props
+    console.log('RENDERING', token)
 
-  return (
-    <iframe
-      title='Mosaico'
-      ref={props.windowRef}
-      id='mosaico-iframe'
-      frameBorder={0}
-      src={src}
-      style={{ width: '100%', height: '760px' }}
-      onLoad={() => console.log('iframe loaded', props.windowRef.current.src)}
-    />
-  )
+    const src =
+      template && template.id
+        ? `${config.mosaicoBasePath}/appeditor.html?bearer=${token}&id=${template.id}`
+        : `${config.mosaicoBasePath}/app.html?bearer=${token}`
+
+    return (
+      <iframe
+        title='Mosaico'
+        ref={windowRef}
+        id='mosaico-iframe'
+        frameBorder={0}
+        src={src}
+        style={{ width: '100%', height: '760px' }}
+        onLoad={() => console.log('iframe loaded', windowRef.current.src)}
+      />
+    )
+  }
 }
 
 // src={`http://localhost:8000/mosaico/appeditor.html?bearer=${token}#/static/mosaico/templates/versafix-1/template-versafix-1.html`}
 
+const mapStateToProps = state => ({ token: state.auth.token })
+
 Mosaico.propTypes = {
   windowRef: PropTypes.object,
-  template: PropTypes.object
+  template: PropTypes.object,
+  token: PropTypes.string
 }
 
-export default React.memo(Mosaico)
+export default connect(mapStateToProps, null)(Mosaico)
