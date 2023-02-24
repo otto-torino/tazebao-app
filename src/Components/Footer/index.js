b750dc8bdb54adf3e37603ac24a0c285183af238
abidibo <abidibo@gmail.com>
2023-02-24 10:42:41 +0100

    Add dynamic copyright year

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,13 +1,15 @@
+import moment from 'moment'
 import React from 'react'
 import Log8 from '../../Assets/img/log8_rosso.png'
 
 import styles from './Footer.module.scss'
 
 const Footer = props => {
+  const year = moment().year()
   return (
     <footer className={styles.footer}>
       <div className={styles.text}>
-        Tazebao © 2016-2021
+        Tazebao © 2016-{year}
         <a href='https://www.otto.to.it' target='_blank' rel='noopener noreferrer'>
           <img src={Log8} className={styles.logo} alt='Otto Srl' />
         </a>
